6346b09a1cf25b1e87958cb42112192477e4a843
daniel <handsomeanewi98@gamil.com>
2024-12-12 17:17:46 +0800

    20241210

---
+++ demo-helloworld/DemoComparison/src/DemoComparsion.java
@@ -0,0 +1,42 @@
+public class DemoComparsion {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        float happy = 102.55f;
+        System.out.println(happy);
+
+       System.out.print("vincent was handsome =");
+
+        int age = 30;
+       boolean result = age <= 30;
+       System.out.print(result);
+
+
+       int bad = 1;
+       int hard = 2;
+       boolean dif = (bad >= 99 && hard <= 99);
+       System.out.println(dif);
+       
+       int x11 =3;
+       int y11 = (x11++ + 3) * x11++;
+       System.out.println(y11);
+       System.out.print(x11);
+
+       int x1 = 9;
+       int x3 = (x1++ +100) *9999;
+       System.out.println(x1);
+       System.out.println(x3);
+      
+       String java = ("up");
+       boolean isjavgood = true;
+       boolean isjavbad = false;
+       System.out.println(java);
+       System.out.println(isjavgood || isjavbad);
+       System.out.println(isjavgood & isjavbad);
+       System.out.println();
+
+       
+
+     }
+   }
+ 
