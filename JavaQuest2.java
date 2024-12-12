9bd312a241f92f04db4cea1ca4437d271a007636
daniel <handsomeanewi98@gamil.com>
2024-12-12 17:22:33 +0800

    20241210

---
+++ JavaQuest2.java
@@ -0,0 +1,32 @@
+/**
+ * Expected Output:
+ * 
+ * a > 9
+ * str length < 27
+ */
+// Primitive types and operators
+public class JavaQuest2 {
+  public static void main(String[] args) {
+    int a = 10;
+    if (a == 9) {
+      System.out.println("a is equals to 9");
+    } else if (a > 9) {
+      System.out.println("a is between 0 and 8");
+     if (a < 9);
+     System.out.println("a < 9");
+    } else if (a < 0) {
+      System.out.println("a < 0");
+    }
+
+    String str = "I love Java. I love coding";
+      int length = str.length();
+      System.out.println("str length = 27");
+     if (length < 27) { 
+      System.out.println("str length < 27");
+    } else if (length > 27) {
+      System.out.println("str length > 27");
+
+      
+    }
+  }
+}
