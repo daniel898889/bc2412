be12ee1f37b7e7fcd8f37e3131bf65eaac9c3d85
daniel <handsomeanewi98@gamil.com>
2024-12-17 01:00:08 +0800

    20241210

---
+++ JavaQuest7.java
@@ -0,0 +1,20 @@
+/**
+ * The sum is 55
+ */
+public class JavaQuest7 {
+  // Sum values of an array
+  public static void main(String[] args) {
+    // Declare a int array, with value 1 to 10
+    // code here
+    int sum = 0;
+    
+  int [] array = {1,2,3,4,5,6,7,8,9,10};
+  int length = array.length;
+  for (int i = 0; i < length; i++) { 
+      if (array[i] <= 55) {
+        sum = sum + array[i];
+      }}
+    
+    System.out.println("The sum is " + sum);
+  }
+}
