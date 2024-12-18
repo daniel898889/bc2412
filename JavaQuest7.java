1c12cfb23cdc4c6b2102882117f55a12fb874c6e
daniel <handsomeanewi98@gamil.com>
2024-12-19 00:54:59 +0800

    20241210

--- JavaQuest7.java
+++ JavaQuest7.java
@@ -6,10 +6,10 @@ public class JavaQuest7 {
   public static void main(String[] args) {
     // Declare a int array, with value 1 to 10
     // code here
-    int sum = 0;
+  int sum = 0;
     
-  int [] array = {1,2,3,4,5,6,7,8,9,10};
-  int length = array.length;
+  int[] array = {1,2,3,4,5,6,7,8,9,10};
+  int  length = array.length;
   for (int i = 0; i < length; i++) { 
       if (array[i] <= 55) {
         sum = sum + array[i];
