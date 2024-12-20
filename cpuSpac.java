c98baf764e721153484e0f0f42c0c7b86a6f83e2
daniel <handsomeanewi98@gamil.com>
2024-12-20 17:04:12 +0800

    20241210

---
+++ cpuSpac.java
@@ -0,0 +1,60 @@
+public class cpuSpac {
+    private int hight ;
+    private int length;
+    private String name;
+    private int width;
+    private float Processor;
+    private int threads;
+    private boolean boost;
+    private int ram;
+
+
+     public  cpuSpac (int hight,int length , String name , int width, float Processor, int threads, boolean boost, int ram) {
+    this.hight = hight; {
+    this.length = length;
+    this.name = name ;  
+    this.width = width; 
+    this.Processor = Processor;
+    this.threads = threads; 
+    this.boost = boost; 
+    this.ram = ram;
+}}
+     public int gethight() {
+        return this.hight; }
+    
+
+    public int getlenght() {
+        return this.length; }
+    
+
+    public String getname() {
+        return this.name;  } 
+
+    public int getwidth(){
+        return this.width;
+    }
+
+    public float getProcessor;{
+        return this.Processor;
+    }
+    public int getthreads(){
+        return this.threads;
+    }
+    public int ram ;{
+        return this.ram;
+    }
+
+    public boolean getboost; {
+    return this.boost;
+    } 
+
+     }
+
+
+
+
+
+    
+
+      
+
